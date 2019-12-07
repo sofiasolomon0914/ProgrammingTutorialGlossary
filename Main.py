94a17cffc17cbbafe2f0b098c8ad70a6191a5125
sofiasolomon0914 <ssolomon2770@eagle.fgcu.edu>
2019-12-07 11:10:43 -0500

    Started

--- Main.py
+++ Main.py
@@ -1,12 +1,29 @@
-# Programming Help Guide Project
+"""
+Programming Help Guide Project
+"""
 
 from tkinter import *
+
+
+# Key down function
+def click():
+    entered_text = textentry.get()
+
 
 # main:
 window = Tk()
 window.title("Programming Help Guide")
 window.configure(background="white")
 
-# Create Welcome Page
-Label(window, text="Welcome to Programming Help Guide!\nYou can use this program to search for tutorials and "
-                   "programming definitions.", bg="white", fg="black", font=("times", 150, "bold"))
+# Create label
+Label(window, text="Enter the tutorial you would "
+                   "like: ", bg="white", font="none 12 bold").grid(row=1, column=0, sticky=W)
+
+# Create text entry box
+textentry = Entry(window, width=20, bg="white")
+textentry.grid(row=2, column=0, sticky=W)
+
+# Create a submit button
+Button(window, text="Submit", width=6, command=click) .grid(row=3, column=0, sticky=W)
+
+window.mainloop()
